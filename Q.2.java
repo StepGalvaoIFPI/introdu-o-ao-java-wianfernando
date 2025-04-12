f78a1a9fa1b8eda1770348e794b7bebb3b077d1d
Wian Fernando <150176463+wianfernando@users.noreply.github.com>
2025-04-12 12:25:49 -0300

    Create Q.2.java

---
+++ Q.2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Q2 {
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite a altura:");
+        double altura = Double.valueOf(entrada.nextDouble());
+
+        System.out.println("Digite a largura:");
+        double largura = Double.valueOf(entrada.nextDouble());
+
+        double area = (altura * largura);
+        double perimetro = (2 * altura + 2 * largura);
+
+        System.out.println("Area =" +area+ "");
+        System.out.println("perimetro =" +perimetro+ "");
+
+        entrada.close();
+    }
+}
