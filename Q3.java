ea4f210352249312b71fea89fb8a67bf0b3125bd
Wian Fernando <150176463+wianfernando@users.noreply.github.com>
2025-04-12 12:26:30 -0300

    Create Q3.java

---
+++ Q3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Q3 {
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite o raio:");
+        double raio = Double.valueOf(entrada.nextDouble());
+        double pi = 3.14;
+
+        double area = (pi * raio * raio);
+        double comprimento = (2 * pi * raio);
+
+        System.out.println("Area = " +area+ "");
+        System.out.println("Comprimento = " +comprimento+ "");
+
+        entrada.close();
+    }
+}
